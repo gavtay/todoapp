d984ad4d0008330214ffbe300bedde7d23169deb
Kataneki <gavintaylor.ai@gmail.com>
2023-12-16 15:22:55 -0600

    Created the base layout of the menu section.

---
+++ src/Project.jsx
@@ -0,0 +1,13 @@
+import './Project.css';
+
+function Project() {
+    return (
+        <>
+            <div id="project-container">
+                <p>^ Project Name</p>
+                <p id='project-task-num'>6</p>
+            </div>
+        </>
+    )
+}
+export default Project;
--- src/menu/Menu.jsx
+++ src/menu/Menu.jsx
@@ -1,9 +1,26 @@
+import Project from '../Project';
 import './Menu.css';
 
 function Menu() {
     return (
         <>
-            <h1>Search</h1>
+            <div id='menu-container'>
+                <div id='search-container'>
+                    <input type='text' id='search-input' placeholder='Icon Search'></input>
+                </div>
+                <div id='projects-container'>
+                    <div id='projects-head-btn-container'>
+                        <h2 id='projects-header'>Projects</h2>
+                        <button id='projects-add-btn'>+</button>
+                    </div>
+                    <div>
+                        <Project />
+                        <Project />
+                        <Project />
+                    </div>
+                </div>
+                <button id='projects-archived-btn'>Archived Projects</button>
+            </div>
         </>
     )
 }
